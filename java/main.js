6cfa0094432824a80799bc73f0230662870c2945
coubston <54748503+coubston@users.noreply.github.com>
2021-07-08 15:21:29 +0700

    Add files via upload

---
+++ java/main.js
@@ -0,0 +1,60 @@
+const headerCityButton = document.querySelector('.header__city-button');
+
+if(localStorage.getItem('lomoda-location')){
+    headerCityButton.textContent = localStorage.getItem('lomoda-location');
+}
+
+headerCityButton.addEventListener('click', () => {
+  const city = prompt('Укажите ваш город!');  
+  headerCityButton.textContent = city;
+  localStorage.setItem('lomoda-location', city)
+});
+
+//bloakc scroll
+
+const disableScroll = () => {
+    const widthScroll = window.innerWidth - document.body.offsetWidth;
+
+    document.body.dbScrollY = window.scrollY;
+
+    document.body.style.cssText = `
+        position: fixed;
+        top: ${-window.scrollY}px;
+        left: 0;
+        width: 100%;
+        height: 100vh;
+        overflow: hidden;
+        padding-right: ${widthScroll}px;
+    `;
+};
+const enableScroll = () => {
+    document.body.style.cssText = '';
+    window.scroll({
+        top:document.body.dbScrollY,
+    });
+};
+
+// modal window
+
+const subheaderCart = document.querySelector('.subheader__cart');
+const cartOverlay = document.querySelector('.cart-overlay');
+
+const cartModalOpen = () => {
+    cartOverlay.classList.add('cart-overlay-open');
+    disableScroll();
+};
+const cartModalClose = () => {
+    cartOverlay.classList.remove('cart-overlay-open');
+    enableScroll();
+};
+
+subheaderCart.addEventListener('click', cartModalOpen);
+
+cartOverlay.addEventListener('click', event => {
+  const target = event.target;
+
+    if(target.classList.contains('cart__btn-close') || target.classList.contains('cart-overlay')){
+        cartModalClose();
+    }
+});
+
